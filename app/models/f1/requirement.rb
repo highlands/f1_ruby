9541956a94b50957faac48ba6dc9e17341fe717b
J.D. Warren <johndavid400@gmail.com>
2016-04-12 19:53:51 -0500

    add requirement model

---
+++ app/models/f1/requirement.rb
@@ -0,0 +1,21 @@
+module F1
+  class Requirement < Authenticate
+    attr_accessor :attrs, :person_id, :requirement_id
+
+    def initialize(test = !Rails.env.production?)
+      @test = test
+      username = ENV["F1_API_USER_USERNAME"]
+      password = ENV["F1_API_USER_PASSWORD"]
+      url = base_url + "PortalUser/AccessToken"
+      data = "ec=" + signature(username, password)
+      post_auth!(url, data)
+    rescue
+      @errors = "Connection Failed"
+    end
+
+    def get_requirements_for(user_id)
+      get!(base_url + "People/#{user_id}/Requirements.json")
+    end
+
+  end
+end
